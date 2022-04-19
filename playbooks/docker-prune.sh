22503b6502cc0a0f16c7dafd8d88d866585d6a8d
Tom McGinnis <git@mcginnis.nu>
2022-04-18 21:18:54 -0400

    another command

--- playbooks/docker-prune.sh
+++ playbooks/docker-prune.sh
@@ -1,1 +1,4 @@
-docker system prune -f --volumes --filter "label!=io.kubernetes.container.name=istio-init"'
+#docker system prune -f --volumes --filter "label!=io.kubernetes.container.name=istio-init"'
+
+docker ps -a --filter exited=0 --format "{{.ID}}\t{{.Names}}"
+
